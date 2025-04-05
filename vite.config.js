16eed94a014e922bd06904e8e646ee98fd136c1d
culeador-extremo <david.qr551@gmail.com>
2025-04-04 21:41:07 -0600

    Fix rutas para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/inventario/",
   plugins: [react()],
-  base: '/inventario/',
   server: {
     host: true,
     port: 5173,
